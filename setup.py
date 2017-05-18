15c9471c9af9c4c9b367f8702819782f1c06bc8b
Mike DeLaurentis <miked@stitchdata.com>
2017-05-18 10:30:27 -0400

    Add boilerplate stuff

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-mysql',
+      version='0.1.0',
+      description='Singer.io tap for extracting data from MySQL',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_mysql'],
+      install_requires=[
+          'attrs==16.3.0',
+          'pendulum==1.2.0',
+          'singer-python==1.6.0a2',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-mysql=tap_mysql:main
+      ''',
+      packages=['tap_mysql'],
+)
