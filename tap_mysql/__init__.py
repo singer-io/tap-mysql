8f70a6a027217124db53846f59f930f002b9a9b9
Mike DeLaurentis <miked@stitchdata.com>
2017-06-23 12:04:24 -0400

    Factor out resolve_catalog

--- tap_mysql/__init__.py
+++ tap_mysql/__init__.py
@@ -356,8 +356,7 @@ def primary_key_columns(connection, db, table):
         return set([c[0] for c in cur.fetchall()])
 
 
-# TODO: Generalize this. Use tap_stream_id rather than database and table.
-# Maybe make it a method on Catalog or CatalogEntry.
+# TODO: Move this into a common library.
 def desired_columns(selected, table_schema):
 
     '''Return the set of column names we need to include in the SELECT.
@@ -484,34 +483,36 @@ def sync_table(connection, catalog_entry, state):
             yield activate_version_message
 
 
-def generate_messages(con, catalog, raw_state):
-    discovered_catalog = discover_catalog(con)
-    state = State.from_dict(raw_state, catalog)
+def resolve_catalog(con, catalog, state):
 
+    discovered = discover_catalog(con)
+
+    # Filter catalog to include only selected streams
     streams = list(filter(lambda stream: stream.is_selected(), catalog.streams))
-    LOGGER.info('%d streams total, %d are selected', len(catalog.streams), len(streams))
-    LOGGER.info('State is %s', state.make_state_message())
-    if state.current_stream:
-        streams = dropwhile(lambda s: s.tap_stream_id != state.current_stream, streams)
 
+    # If the state says we were in the middle of processing a stream, skip
+    # to that stream.
+    if state.current_stream:
+        streams = dropwhile(lambda s: s.tap_stream_id != state.current_stream, streams)    
+
+    result = Catalog(streams=[])
+        
     # Iterate over the streams in the input catalog and match each one up
     # with the same stream in the discovered catalog.
     for catalog_entry in streams:
-        state.current_stream = catalog_entry.tap_stream_id
-        yield state.make_state_message()
 
-        discovered_table = discovered_catalog.get_stream(catalog_entry.tap_stream_id)
+        discovered_table = discovered.get_stream(catalog_entry.tap_stream_id)
         if not discovered_table:
             LOGGER.warning('Database %s table %s was selected but does not exist',
                            catalog_entry.database, catalog_entry.table)
-
+            next
         selected = set([k for k, v in catalog_entry.schema.properties.items()
                         if v.selected or k == catalog_entry.replication_key])
 
         # These are the columns we need to select
         columns = desired_columns(selected, discovered_table.schema)
 
-        catalog_entry = CatalogEntry(
+        result.streams.append(CatalogEntry(
             tap_stream_id=catalog_entry.tap_stream_id,
             key_properties=catalog_entry.key_properties,
             stream=catalog_entry.stream,
@@ -523,8 +524,20 @@ def generate_messages(con, catalog, raw_state):
                 properties={col: discovered_table.schema.properties[col]
                             for col in columns}
             )
-        )
+        ))
 
+    return result
+
+        
+def generate_messages(con, catalog, raw_state):
+    state = State.from_dict(raw_state, catalog)
+    catalog = resolve_catalog(con, catalog, state)
+    
+    # Iterate over the streams in the input catalog and match each one up
+    # with the same stream in the discovered catalog.
+    for catalog_entry in catalog.streams:
+        state.current_stream = catalog_entry.tap_stream_id
+        yield state.make_state_message()
         yield singer.SchemaMessage(
             stream=catalog_entry.stream,
             schema=catalog_entry.schema.to_dict(),
