e81d7f4be428158991c96c00363fa5640ab32e85
John Miller <jmiller@rjmetrics.com>
2017-07-21 13:42:42 -0400

    support for google cloud mysql and ssl

--- tap_mysql/__init__.py
+++ tap_mysql/__init__.py
@@ -25,6 +25,7 @@ from singer import utils
 from singer.schema import Schema
 from singer.catalog import Catalog, CatalogEntry
 
+
 Column = collections.namedtuple('Column', [
     "table_schema",
     "table_name",
@@ -48,8 +49,37 @@ LOGGER = singer.get_logger()
 CONNECT_TIMEOUT_SECONDS = 300
 READ_TIMEOUT_SECONDS = 3600
 
+# We need to hold onto this for self-signed SSL
+match_hostname = ssl.match_hostname
+
+
+def parse_internal_hostname(hostname):
+    # special handling for google cloud
+    if ":" in hostname:
+        parts = hostname.split(":")
+        if len(parts) == 3:
+            return parts[0] + ":" + parts[2]
+        else:
+            return parts[0] + ":" + parts[1]
+
+    return hostname
+
 
 def open_connection(config):
+    # Google Cloud's SSL involves a self-signed certificate. This certificate's
+    # hostname matches the form {instance}:{box}. The hostname displayed in the
+    # Google Cloud UI is of the form {instance}:{region}:{box} which
+    # necessitates the "parse_internal_hostname" function to get the correct
+    # hostname to match.
+    # The "internal_hostname" config variable allows for matching the SSL
+    # against a host that doesn't match the host we are connecting to. In the
+    # case of Google Cloud, we will be connecting to an IP, not the hostname
+    # the SSL certificate expects.
+    # The "ssl.match_hostname" function is patched to check against the
+    # internal hostname rather than the host of the connection. In the event
+    # that the connection fails, the patch is reverted by reassigning the
+    # patched out method to it's original spot.
+
     args = {
         "user": config["user"],
         "password": config["password"],
@@ -63,13 +93,27 @@ def open_connection(config):
     if config.get("database"):
         args["database"] = config["database"]
 
-    if config.get("ssl_ca"):
+    # Attempt self-signed SSL if config vars are present
+    if config.get("ssl_ca") and config.get("ssl_cert") and config.get("ssl_key"):
         try:
             LOGGER.info("Attempting SSL connection with custom ca")
-            return pymysql.connect(ssl={"ca": config["ssl_ca"]}, **args)
+            ssl_arg = {
+                "ca": config["ssl_ca"],
+                "cert": config["ssl_cert"],
+                "key": config["ssl_key"],
+            }
+
+            # override match hostname for google cloud
+            if config.get("internal_hostname"):
+                hostname = parse_internal_hostname(config["internal_hostname"])
+                ssl.match_hostname = lambda cert, hostname: match_hostname(cert, hostname)
+
+            return pymysql.connect(ssl=ssl_arg, **args)
         except: # pylint: disable=bare-except
+            ssl.match_hostname = match_hostname
             LOGGER.error("SSL connection with custom ca failed.")
 
+    # Attempt SSL if SSL is enabled
     if config.get("ssl", False):
         try:
             LOGGER.info("Attempting SSL connection")
@@ -84,6 +128,7 @@ def open_connection(config):
         except: # pylint: disable=bare-except
             LOGGER.error("SSL connection failed")
 
+    # Attempt unencrypted connection
     try:
         LOGGER.info("Attempting connection")
         return pymysql.connect(**args)
