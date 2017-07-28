047b109b4463d9f5805d891098569e65e6bd8b2f
Matthew Bilyeu <matt@stitchdata.com>
2017-07-28 09:51:10 -0400

    bump version to 0.8.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mysql',
-      version='0.7.0',
+      version='0.8.0',
       description='Singer.io tap for extracting data from MySQL',
       author='Stitch',
       url='https://singer.io',
