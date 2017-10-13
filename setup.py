01acf42184f08bde49f6412243af51502a412edb
Paul Santa Clara <psantaclara@rjmetrics.com>
2017-10-13 10:26:20 -0400

    burning version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mysql',
-      version='0.12.4',
+      version='0.12.5',
       description='Singer.io tap for extracting data from MySQL',
       author='Stitch',
       url='https://singer.io',
