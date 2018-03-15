b0c11357752eb95861ffc25e48563b75c90782c3
Paul Santa Clara <psantaclara@rjmetrics.com>
2018-03-15 13:59:01 -0400

    always publish table-key-properties even if empty

--- tap_mysql/__init__.py
+++ tap_mysql/__init__.py
@@ -362,8 +362,7 @@ def discover_catalog(connection):
                 s.properties[c.column_name].inclusion != 'unsupported'
             )
             key_properties = [c.column_name for c in cols if column_is_key_prop(c, schema)]
-            if key_properties:
-                md.append({'metadata': {'table-key-properties' : key_properties}, 'breadcrumb': ()})
+            md.append({'metadata': {'table-key-properties' : key_properties}, 'breadcrumb': ()})
 
             if table_schema in table_info and table_name in table_info[table_schema]:
                 entry.row_count = table_info[table_schema][table_name]['row_count']
