5bac530b6f5fe07194311cbb5626cc3f87be7e3b
Anne Maiale <anne@stitchdata.com>
2019-03-28 01:57:51 +0000

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mysql',
-      version='1.12.2',
+      version='1.13.0',
       description='Singer.io tap for extracting data from MySQL',
       author='Stitch',
       url='https://singer.io',
