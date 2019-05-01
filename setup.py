e40b112e8f5c522a2fe3d4cf8b11271cbed22c65
Dan Mosora <dan@stitchdata.com>
2019-05-01 17:07:02 +0000

    Verion 1.13.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mysql',
-      version='1.13.5',
+      version='1.13.6',
       description='Singer.io tap for extracting data from MySQL',
       author='Stitch',
       url='https://singer.io',
