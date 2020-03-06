6e7ff6d6e33de0b79aeb65b4a444171c343f4643
Andy Lu <andy@stitchdata.com>
2020-03-06 19:07:06 +0000

    Bump to v1.16.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mysql',
-      version='1.15.1',
+      version='1.16.0',
       description='Singer.io tap for extracting data from MySQL',
       author='Stitch',
       url='https://singer.io',
