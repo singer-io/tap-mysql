c3f6b8b2c4ff9ffa6d7c774cd3b11182cec2968a
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2020-05-08 10:36:46 -0400

    Use flags to determine if the current log is complete (#115)
    * Use flags to determine if the current log is complete
    
    * Add check for flags on event
    
    * Change format of constant to match docs
    
    Co-authored-by: Nick McCoy <33731945+nick-mccoy@users.noreply.github.com>

--- tap_mysql/sync_strategies/binlog.py
+++ tap_mysql/sync_strategies/binlog.py
@@ -41,6 +41,13 @@ mysql_timestamp_types = {
     FIELD_TYPE.TIMESTAMP2
 }
 
+# Event Flags
+# NB: Event flags are an unsigned short that accounts for flags on the event itself
+# Flag docs: https://dev.mysql.com/doc/internals/en/binlog-event-flag.html
+# - Note: `mysqlbinlog` calls 0x0001 STMT_END_F, which we are using to decide if we need to bookmark
+LOG_EVENT_BINLOG_IN_USE_F = 0x0001 # Corresponds to the end log of a statement, set if log closed
+                                   # successfully after writing
+
 def add_automatic_properties(catalog_entry, columns):
     catalog_entry.schema.properties[SDC_DELETED_AT] = Schema(
         type=["null", "string"],
@@ -371,10 +378,12 @@ def _run_binlog_sync(mysql_conn, reader, binlog_streams_map, state):
                                 binlog_event.schema,
                                 binlog_event.table)
 
-        state = update_bookmarks(state,
-                                 binlog_streams_map,
-                                 reader.log_file,
-                                 reader.log_pos)
+        # NB: Flag 0x1 indicates that the binlog has been closed successfully, so we can rely on this being a complete log.
+        if hasattr(binlog_event, 'flags') and binlog_event.flags & LOG_EVENT_BINLOG_IN_USE_F:
+            state = update_bookmarks(state,
+                                     binlog_streams_map,
+                                     reader.log_file,
+                                     reader.log_pos)
 
         # The iterator across python-mysql-replication's fetchone method should ultimately terminate
         # upon receiving an EOF packet. There seem to be some cases when a MySQL server will not send
